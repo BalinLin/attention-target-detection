cef7fbcbdd66f5df95f90cbe892bb4e4bec76561
BalinLin <giveme2486@gmail.com>
2021-12-12 17:04:06 +0800

    GazeNet_3

--- model.py
+++ model.py
@@ -121,7 +121,7 @@ class ModelSpatial(nn.Module):
         # self.gamma_fovdepth = nn.Parameter(torch.zeros(1))
 
         # gaze direction
-        self.gaze = GazeTR()
+        self.gaze = GazeNet()
 
         # scene pathway, input size = 8 means Scene Image cat with fov, depth map and Head Position
         self.conv1_scene = nn.Conv2d(8, 64, kernel_size=7, stride=2, padding=3, bias=False)
@@ -214,7 +214,7 @@ class ModelSpatial(nn.Module):
         return nn.Sequential(*layers)
 
 
-    def forward(self, images, depth, head, face, face_depth, gaze_field, device):
+    def forward(self, images, depth, head, face, face_depth, gaze_field, eye):
         ### images -> whole image(Scene Image), head -> position image(Head Position), face -> head image(Cropped Head)
         # images.shape -> torch.Size([batch_size, 3, 224, 224]),
         # depth.shape -> torch.Size([batch_size, 1, 224, 224]),
@@ -225,7 +225,7 @@ class ModelSpatial(nn.Module):
         # eye.shape -> torch.Size([batch_size, 2])
         # gaze.shape -> torch.Size([batch_size, 2])
 
-        direction = self.gaze(face, device) # (N, 3, 224, 224) -> (N, 3)
+        direction = self.gaze(face, eye) # (N, 3, 224, 224) -> (N, 3)
 
         # infer gaze direction and normalized
         norm = torch.norm(direction[:, :2], 2, dim=1)
@@ -552,119 +552,31 @@ class ModelSpatioTemporal(nn.Module):
 def _get_clones(module, N):
     return nn.ModuleList([copy.deepcopy(module) for i in range(N)])
 
-class TransformerEncoder(nn.Module):
-
-    def __init__(self, encoder_layer, num_layers, norm=None):
-        super().__init__()
-        self.layers = _get_clones(encoder_layer, num_layers)
-        self.num_layers = num_layers
-        self.norm = norm
-
-    def forward(self, src, pos):
-        output = src
-        for layer in self.layers:
-            output = layer(output, pos)
-
-        if self.norm is not None:
-            output = self.norm(output)
-
-        return output
-
-
-class TransformerEncoderLayer(nn.Module):
-
-    def __init__(self, d_model, nhead, dim_feedforward=512, dropout=0.1):
-        super().__init__()
-        self.self_attn = nn.MultiheadAttention(d_model, nhead, dropout=dropout)
-        # Implementation of Feedforward model
-        self.linear1 = nn.Linear(d_model, dim_feedforward)
-        self.dropout = nn.Dropout(dropout)
-        self.linear2 = nn.Linear(dim_feedforward, d_model)
-
-        self.norm1 = nn.LayerNorm(d_model)
-        self.norm2 = nn.LayerNorm(d_model)
-
-        self.dropout1 = nn.Dropout(dropout)
-        self.dropout2 = nn.Dropout(dropout)
-
-        self.activation = nn.ReLU(inplace=True)
-
-    def pos_embed(self, src, pos):
-        batch_pos = pos.unsqueeze(1).repeat(1, src.size(1), 1)
-        return src + batch_pos
-
-
-    def forward(self, src, pos):
-                # src_mask: Optional[Tensor] = None,
-                # src_key_padding_mask: Optional[Tensor] = None):
-                # pos: Optional[Tensor] = None):
-
-        q = k = self.pos_embed(src, pos)
-        src2 = self.self_attn(q, k, value=src)[0]
-        src = src + self.dropout1(src2)
-        src = self.norm1(src)
-
-        src2 = self.linear2(self.dropout(self.activation(self.linear1(src))))
-        src = src + self.dropout2(src2)
-        src = self.norm2(src)
-        return src
-
-class GazeTR(nn.Module):
+class GazeNet(nn.Module):
     def __init__(self):
-        super(GazeTR, self).__init__()
-        maps = 32
-        nhead = 8
-        dim_feature = 7*7
-        dim_feedforward=512
-        dropout = 0.1
-        num_layers=6
+        super(GazeNet, self).__init__()
+        self.face_net = resnet18(pretrained=False, maps=512)
 
-        self.base_model = resnet18(pretrained=False, maps=maps)
+        self.eye_position_transform = nn.Sequential(nn.Linear(2, 256),
+                                                    nn.ReLU(inplace=True))
 
-        # d_model: dim of Q, K, V
-        # nhead: seq num
-        # dim_feedforward: dim of hidden linear layers
-        # dropout: prob
+        self.fusion = nn.Sequential(nn.Linear(512 + 256, 256),
+                                    nn.ReLU(inplace=True),
+                                    nn.Linear(256, 3))
 
-        encoder_layer = TransformerEncoderLayer(
-                  maps,
-                  nhead,
-                  dim_feedforward,
-                  dropout)
+        self.relu = nn.ReLU(inplace=False)
+        self.avgpool = nn.AvgPool2d(7, stride=1)
 
-        encoder_norm = nn.LayerNorm(maps)
-        # num_encoder_layer: deeps of layers
+    def forward(self, face_image, eye_position):
+        # face part forward
+        face_feature = self.face_net(face_image)
+        face_feature = self.avgpool(face_feature).view(-1, 512)
 
-        self.encoder = TransformerEncoder(encoder_layer, num_layers, encoder_norm)
+        # eye position transform
+        eye_feature = self.eye_position_transform(eye_position)
 
-        self.cls_token = nn.Parameter(torch.randn(1, 1, maps))
+        # fusion
+        feature = torch.cat((face_feature, eye_feature), 1)
+        direction = self.fusion(feature)
 
-        self.pos_embedding = nn.Embedding(dim_feature+1, maps)
-
-        self.feed = nn.Linear(maps, 3)
-
-        self.loss_op = nn.L1Loss()
-
-    def forward(self, x_in, device):
-        feature = self.base_model(x_in) # (N, 32, 7, 7)
-        batch_size = feature.size(0)
-        feature = feature.flatten(2)
-        feature = feature.permute(2, 0, 1)
-
-        cls = self.cls_token.repeat( (1, batch_size, 1))
-        feature = torch.cat([cls, feature], 0)
-
-        position = torch.from_numpy(np.arange(0, 50)).to(device)
-
-        pos_feature = self.pos_embedding(position)
-
-        # feature is [HW, batch, channel]
-        feature = self.encoder(feature, pos_feature)
-
-        feature = feature.permute(1, 2, 0)
-
-        feature = feature[:,:,0]
-
-        gaze = self.feed(feature)
-
-        return gaze
+        return direction
